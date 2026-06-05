2328535c6ebff8bf94968cd8ec3db19647b12008
jayy <jegedejeremiah12@gmail.com>
2026-06-05 14:36:59 +0100

    removed dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/ui/ThemeProvider";
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 export const metadata: Metadata = {
@@ -21,18 +20,11 @@ export default function RootLayout({
       <html lang="en"  suppressHydrationWarning className={`antialiased`}>
         <body className="font-poppins antialiased">
            <NuqsAdapter>
-             <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
           <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-1">{children}</main>
             <Footer />
           </div>
-          </ThemeProvider>
           </NuqsAdapter>
         </body>
       </html>
